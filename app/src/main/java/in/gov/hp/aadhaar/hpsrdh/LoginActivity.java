b2319d089725e048dabd0adeaf9a580a9fcdf0fa
KushKD <kushkumardhawan@gmail.com>
2015-06-10 13:19:12 +0530

    This is the Seventh Commit . Added Splash Screen , and the Login Screen . Integrated the graphics and the colour scheme . Currently working on the Sign in Screen.

--- app/src/main/java/in/gov/hp/aadhaar/hpsrdh/LoginActivity.java
+++ app/src/main/java/in/gov/hp/aadhaar/hpsrdh/LoginActivity.java
@@ -2,12 +2,15 @@ package in.gov.hp.aadhaar.hpsrdh;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,15 +19,25 @@ public class LoginActivity extends Activity {
 
     String IMIE_Number = null;
     TextView tv_IMEI ;
+    RelativeLayout button_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         tv_IMEI = (TextView)findViewById(R.id.tv_imei);
+        button_main = (RelativeLayout)findViewById(R.id.button_main);
 
       IMIE_Number = getIMEI(LoginActivity.this);
         tv_IMEI.setText(IMIE_Number);
+
+        button_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this , SignIn.class );
+                startActivity(i);
+            }
+        });
     }
 
 
---
+++ app/src/main/java/in/gov/hp/aadhaar/hpsrdh/SplashScreen.java
@@ -0,0 +1,33 @@
+package in.gov.hp.aadhaar.hpsrdh;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by admin on 10/06/2015.
+ */
+public class SplashScreen extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, LoginActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
