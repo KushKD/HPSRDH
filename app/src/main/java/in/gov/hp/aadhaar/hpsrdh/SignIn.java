125ba6eeada72ce9501b81e3f277ea6267609bf8
KushKD <kushkumardhawan@gmail.com>
2015-06-11 15:13:49 +0530

    This the Ninth commit.
    Added the Two screens with advances Search and  Basic Search . Added the two animations . Working on icons

--- app/src/main/java/in/gov/hp/aadhaar/hpsrdh/SignIn.java
+++ app/src/main/java/in/gov/hp/aadhaar/hpsrdh/SignIn.java
@@ -1,18 +1,33 @@
 package in.gov.hp.aadhaar.hpsrdh;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SignIn extends Activity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+      login = (Button)findViewById(R.id.button_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i_2 = new Intent(SignIn.this , ViewPagerStyle1Activity.class);
+                startActivity(i_2);
+            }
+        });
+
     }
 
 
