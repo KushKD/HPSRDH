eb30b03d1240608454f063dec61d0863fa0cb505
KushKD <kushkumardhawan@gmail.com>
2015-06-15 12:48:30 +0530

    This is the Twelvth commit. Added primary Code . Login Code Added .Just need the service . Added the Layout one code . Added Districts , Date of Birth Picker and change d the layout wirdth of the edit text .
     Waiting for the Service

--- app/src/main/java/in/gov/hp/aadhaar/hpsrdh/LayoutOne.java
+++ app/src/main/java/in/gov/hp/aadhaar/hpsrdh/LayoutOne.java
@@ -1,14 +1,48 @@
 
 package in.gov.hp.aadhaar.hpsrdh;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class LayoutOne extends Fragment {
+
+	ArrayAdapter<String> districts = null;
+	EditText  name , fHname , pincode;
+	TextView date_of_birth;
+	Spinner district;
+	Button SearchService;
+
+	// variables to store the selected date and time
+	private int mSelectedYear;
+	private int mSelectedMonth;
+	private int mSelectedDay;
+
+	// CallBacks for date and time pickers
+	private DatePickerDialog.OnDateSetListener mOnDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+			// update the current variables ( year, month and day)
+			mSelectedDay = dayOfMonth;
+			mSelectedMonth = monthOfYear;
+			mSelectedYear = year;
+
+			// update txtDate with the selected date
+			updateDateUI();
+		}
+	};
+
 
 
 	public static Fragment newInstance(Context context) {
@@ -19,8 +53,51 @@ public class LayoutOne extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.layout_one, null);		
+
+
+		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.layout_one, null);
+
+		district = (Spinner)root.findViewById(R.id.edit_text_district);
+        SearchService = (Button)root.findViewById(R.id.button_search);
+
+
+		date_of_birth = (TextView)root.findViewById(R.id.edit_text_date_of_birth);
+		name = (EditText)root.findViewById(R.id.edit_text_name);
+		fHname = (EditText)root.findViewById(R.id.edit_text_father_husband_name);
+		pincode = (EditText)root.findViewById(R.id.edit_text_pincode);
+
+		districts = new ArrayAdapter<String>(getActivity(), R.layout.spinner_item, getResources().getStringArray(R.array.district));
+		district.setAdapter(districts);
+
+
+		date_of_birth.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showDatePickerDialog(mSelectedYear, mSelectedMonth, mSelectedDay, mOnDateSetListener);
+			}
+		});
+
+		SearchService.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+
 		return root;
 	}
-	
+	// initialize the DatePickerDialog
+	private DatePickerDialog showDatePickerDialog(int initialYear, int initialMonth, int initialDay, DatePickerDialog.OnDateSetListener listener) {
+		DatePickerDialog dialog = new DatePickerDialog(getActivity(), listener, initialYear, initialMonth, initialDay);
+		dialog.setTitle("Please Select Your Date Of Birth");
+		dialog.show();
+		return dialog;
+	}
+
+	private void updateDateUI() {
+		String month = ((mSelectedMonth+1) > 9) ? ""+(mSelectedMonth+1): "0"+(mSelectedMonth+1) ;
+		String day = ((mSelectedDay) < 10) ? "0"+mSelectedDay: ""+mSelectedDay ;
+		date_of_birth.setText(day + "/" + month + "/" + mSelectedYear);
+	}
 }
--- app/src/main/java/in/gov/hp/aadhaar/hpsrdh/SignIn.java
+++ app/src/main/java/in/gov/hp/aadhaar/hpsrdh/SignIn.java
@@ -2,34 +2,83 @@ package in.gov.hp.aadhaar.hpsrdh;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class SignIn extends Activity {
 
     Button login;
+    EditText username , password;
+    public static final String url_loginService = "" ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-      login = (Button)findViewById(R.id.button_login);
+        login = (Button)findViewById(R.id.button_login);
+        username = (EditText)findViewById(R.id.edit_text_username);
+        password = (EditText)findViewById(R.id.editText_password);
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i_2 = new Intent(SignIn.this , ViewPagerStyle1Activity.class);
-                startActivity(i_2);
-                SignIn.this.finish();
+
+                String user_Name = username.getText().toString().trim();
+                String pass_Word = password.getText().toString().trim();
+
+                logIn login_server = new logIn();
+                login_server.execute(user_Name ,pass_Word);
+
+
             }
         });
 
+
+
     }
 
+  protected class logIn extends AsyncTask<String,String,String>{
+
+      @Override
+      protected void onPreExecute() {
+          super.onPreExecute();
+      }
+
+      @Override
+      protected String doInBackground(String... params) {
+
+          String username_service =  params[0];
+          String password_service = params[1];
+          StringBuilder sb = new StringBuilder();
+          sb.append(url_loginService);
+          sb.append("/");
+          sb.append(username_service);
+          sb.append("/");
+          sb.append(password_service);
+
+         Log.d("Service is" , sb.toString());
+
+            return  null;
+      }
+
+      @Override
+      protected void onPostExecute(String s) {
+          super.onPostExecute(s);
+
+           Intent i_2 = new Intent(SignIn.this , ViewPagerStyle1Activity.class);
+           startActivity(i_2);
+           SignIn.this.finish();
+      }
+  }
 
 }
