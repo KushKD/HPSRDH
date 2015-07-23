6127db4279577c76805bc8d346a738f41f04add1
kushkd <kushkumardhawan@gmail.com>
2015-07-23 15:23:00 +0530

    Changed the Layout One and Two

--- app/src/main/java/in/gov/hp/aadhaar/hpsrdh/LayoutOne.java
+++ app/src/main/java/in/gov/hp/aadhaar/hpsrdh/LayoutOne.java
@@ -14,6 +14,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LayoutOne extends Fragment {
 
@@ -81,12 +82,83 @@ public class LayoutOne extends Fragment {
 			@Override
 			public void onClick(View v) {
 
+				try{
+					//Check Internet Connectivity Weather Connecting or not.
+				Get_Data();
+				}catch (Exception e){
+					Toast.makeText(getActivity(),e.getMessage().toString().trim() ,Toast.LENGTH_LONG).show();
+				}
+
 			}
 		});
 
 
 		return root;
 	}
+
+	private void Get_Data() {
+
+		String District_Service , DOB_Service , Name_Service , FHName_Service , PinCode_Service;
+
+		District_Service = district.getSelectedItem().toString().trim();
+		DOB_Service = date_of_birth.getText().toString().trim();
+		Name_Service = name.getText().toString().trim();
+		FHName_Service = fHname.getText().toString().trim();
+		PinCode_Service = pincode.getText().toString().trim();
+
+		EncryptData ED = new EncryptData();
+
+		if(District_Service.length()!=0 && District_Service!=null){
+			//Encrypt District
+			String District_Crypt_service = ED.Encrypt_String(District_Service);
+
+			if (DOB_Service.length()!=0 && DOB_Service!= null){
+				//Encrypt Dob
+				String DOB_Crypt_Service = ED.Encrypt_String(DOB_Service);
+
+				if(Name_Service.length()!= 0 && Name_Service!= null){
+					//Encrypt Name
+					String Name_Crypt_Service = ED.Encrypt_String(Name_Service);
+
+					if(FHName_Service.length()!= 0 && FHName_Service!= null){
+						//Encrypt Fathers name or Husband name
+						String FH_Crypt_Service = ED.Encrypt_String(FHName_Service);
+
+						if(PinCode_Service.length()!= 0 && PinCode_Service!= null){
+							//Encrypt Pin Code
+							String PinCode_Crypt_Service = ED.Encrypt_String(PinCode_Service);
+
+							//Start Async Task For Four Parameters
+							Toast.makeText(getActivity(),District_Crypt_service +"==="+ DOB_Crypt_Service +"==="+Name_Crypt_Service +"==="+ FH_Crypt_Service +"==="+ PinCode_Crypt_Service,Toast.LENGTH_LONG).show();
+							Toast.makeText(getActivity(),"Async Task For Five Parameters Started",Toast.LENGTH_LONG).show();
+						}else{
+
+							Toast.makeText(getActivity(),"Async Task For Four Parameters Started",Toast.LENGTH_LONG).show();
+							Toast.makeText(getActivity(),District_Crypt_service +"==="+ DOB_Crypt_Service +"==="+Name_Crypt_Service +"==="+ FH_Crypt_Service ,Toast.LENGTH_LONG).show();
+
+							//Start Async Task for Five Parameters
+						}
+					}else{
+						Toast.makeText(getActivity(), "Please enter Your Father's / Husband's Name",Toast.LENGTH_LONG).show();
+					}
+
+				}else{
+					Toast.makeText(getActivity(), "Name cannot be empty",Toast.LENGTH_LONG).show();
+				}
+			}else{
+				Toast.makeText(getActivity(), "Invalid Date OF Birth",Toast.LENGTH_LONG).show();
+			}
+
+		}else{
+			Toast.makeText(getActivity(), "Please Select District",Toast.LENGTH_LONG).show();
+		}
+
+		/**
+		 * Encrypt Data Here
+		 */
+
+	}
+
 	// initialize the DatePickerDialog
 	private DatePickerDialog showDatePickerDialog(int initialYear, int initialMonth, int initialDay, DatePickerDialog.OnDateSetListener listener) {
 		DatePickerDialog dialog = new DatePickerDialog(getActivity(), listener, initialYear, initialMonth, initialDay);
--- app/src/main/java/in/gov/hp/aadhaar/hpsrdh/LayoutTwo.java
+++ app/src/main/java/in/gov/hp/aadhaar/hpsrdh/LayoutTwo.java
@@ -7,20 +7,65 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LayoutTwo extends Fragment {
 
+	EditText UID_ET , Phone_number_ET ;
+	Button BSearch;
+
 
 	public static Fragment newInstance(Context context) {
-		LayoutTwo f = new LayoutTwo();	
+		LayoutTwo f = new LayoutTwo();
+
+
 		
 		return f;
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.layout_two, null);		
+		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.layout_two, null);
+
+		UID_ET = (EditText)root.findViewById(R.id.edit_text_search_by_uid);
+		Phone_number_ET = (EditText)root.findViewById(R.id.edit_text_search_by_phone_number);
+		BSearch = (Button)root.findViewById(R.id.searchButton);
+
+		BSearch.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				try{
+					Search_Data();
+				}catch (Exception e){
+					Toast.makeText(getActivity(),e.getMessage().toString().trim(),Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+
+
 		return root;
 	}
-	
+
+	private void Search_Data() {
+
+		String UID_Service , Phone_Service;
+		EncryptData ED = new EncryptData();
+		UID_Service = UID_ET.getText().toString().trim();
+		Phone_Service = Phone_number_ET.getText().toString().trim();
+
+
+		if(UID_Service.length()!=0 && UID_Service!= null){
+			//Encrypt UID Service
+			String Crypt_UID = ED.Encrypt_String(UID_Service);
+
+		}else{
+
+			Toast.makeText(getActivity(),"Aadhar ID cannto be empty",Toast.LENGTH_LONG).show();
+
+		}
+	}
+
 }
