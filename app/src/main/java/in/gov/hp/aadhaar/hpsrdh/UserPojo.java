f82d1d75729b79c45c90de68d4f769f0a362b415
kushkd <kushkumardhawan@gmail.com>
2015-07-26 17:15:19 +0530

    Added User Pojo Class . Will be adding the Json class On Monday

---
+++ app/src/main/java/in/gov/hp/aadhaar/hpsrdh/UserPojo.java
@@ -0,0 +1,158 @@
+package in.gov.hp.aadhaar.hpsrdh;
+
+import java.io.Serializable;
+
+/**
+ * Created by KD on 7/26/2015.
+ */
+public class UserPojo implements Serializable {
+
+    private String EmailID_User;
+    private String Aadhaar_User;
+    private String Resident_Name_user;
+    private String DOB_User;
+    private String Gender_User;
+    private String Care_OFF_User;
+    private String Address_Building_User;
+    private String Address_Street_User;
+    private String Address_Landmark_User;
+    private String Address_Locality_User;
+    private String District_User;
+    private String State_Name_User;
+    private String Address_Pincode_User;
+    private String Res_Gauardian_User;
+    private String res_addr_subdistrict_name_User;
+    private String res_addr_po_name_User;
+
+    public String getRes_addr_po_name_User() {
+        return res_addr_po_name_User;
+    }
+
+    public void setRes_addr_po_name_User(String res_addr_po_name_User) {
+        this.res_addr_po_name_User = res_addr_po_name_User;
+    }
+
+    public String getAddress_Landmark_User() {
+        return Address_Landmark_User;
+    }
+
+    public void setAddress_Landmark_User(String address_Landmark_User) {
+        Address_Landmark_User = address_Landmark_User;
+    }
+
+    public String getAddress_Locality_User() {
+        return Address_Locality_User;
+    }
+
+    public void setAddress_Locality_User(String address_Locality_User) {
+        Address_Locality_User = address_Locality_User;
+    }
+
+    public String getDistrict_User() {
+        return District_User;
+    }
+
+    public void setDistrict_User(String district_User) {
+        District_User = district_User;
+    }
+
+    public String getState_Name_User() {
+        return State_Name_User;
+    }
+
+    public void setState_Name_User(String state_Name_User) {
+        State_Name_User = state_Name_User;
+    }
+
+    public String getAddress_Pincode_User() {
+        return Address_Pincode_User;
+    }
+
+    public void setAddress_Pincode_User(String address_Pincode_User) {
+        Address_Pincode_User = address_Pincode_User;
+    }
+
+    public String getRes_Gauardian_User() {
+        return Res_Gauardian_User;
+    }
+
+    public void setRes_Gauardian_User(String res_Gauardian_User) {
+        Res_Gauardian_User = res_Gauardian_User;
+    }
+
+    public String getRes_addr_subdistrict_name_User() {
+        return res_addr_subdistrict_name_User;
+    }
+
+    public void setRes_addr_subdistrict_name_User(String res_addr_subdistrict_name_User) {
+        this.res_addr_subdistrict_name_User = res_addr_subdistrict_name_User;
+    }
+
+    public String getDOB_User() {
+        return DOB_User;
+    }
+
+    public void setDOB_User(String DOB_User) {
+        this.DOB_User = DOB_User;
+    }
+
+    public String getGender_User() {
+        return Gender_User;
+    }
+
+    public void setGender_User(String gender_User) {
+        Gender_User = gender_User;
+    }
+
+    public String getCare_OFF_User() {
+        return Care_OFF_User;
+    }
+
+    public void setCare_OFF_User(String care_OFF_User) {
+        Care_OFF_User = care_OFF_User;
+    }
+
+    public String getAddress_Building_User() {
+        return Address_Building_User;
+    }
+
+    public void setAddress_Building_User(String address_Building_User) {
+        Address_Building_User = address_Building_User;
+    }
+
+    public String getAddress_Street_User() {
+        return Address_Street_User;
+    }
+
+    public void setAddress_Street_User(String address_Street_User) {
+        Address_Street_User = address_Street_User;
+    }
+
+    public String getEmailID_User() {
+        return EmailID_User;
+    }
+
+    public void setEmailID_User(String emailID_User) {
+        EmailID_User = emailID_User;
+    }
+
+    public String getAadhaar_User() {
+        return Aadhaar_User;
+    }
+
+    public void setAadhaar_User(String aadhaar_User) {
+        Aadhaar_User = aadhaar_User;
+    }
+
+    public String getResident_Name_user() {
+        return Resident_Name_user;
+    }
+
+    public void setResident_Name_user(String resident_Name_user) {
+        Resident_Name_user = resident_Name_user;
+    }
+
+
+
+
+}
